3c582a1769283cfd7a45c4b6f8408f5c6a2cb017
adamskibicki <adam-skibicki@outlook.com>
2023-04-07 12:53:44 +0200

    Add showing hidden to stats

--- src/CharacterPanel/SideBar/Stats.js
+++ src/CharacterPanel/SideBar/Stats.js
@@ -3,19 +3,24 @@ import GeneralProperty from "../../GeneralProperty";
 import StatpointProperty from "../../StatpointProperty";
 import StatsEditModal from "./StatsEditModal";
 import TitleWithEditButton from "./TitleWithEditButton";
+import InputCheckbox from "../../Inputs/InputCheckbox";
 
 function Stats(props) {
     const [showEditModal, setShowEditModal] = useState(false);
+    const [showHiddenStats, setShowHiddenStats] = useState(false);
+
+    const statsToShow = showHiddenStats ? props.stats : props.stats.filter(s => !s.isHidden);
 
     return (
         <>
             <TitleWithEditButton onEditClick={() => setShowEditModal(true)} title={'Stats'} />
+            <InputCheckbox label={"Show hidden stats"} value={showHiddenStats} onChange={(_, value) => setShowHiddenStats(value)}/>
 
             <div>
                 <GeneralProperty name='Unspent statpoints' value={props.unspentStatpoints} />
                 {
-                    props.stats.map((s, i) => (
-                        <StatpointProperty key={i} name={s.name} value={s.value} increaseEnabled={true} decreaseEnabled={false} calculatedValue={props.calculateFinalStatValue(s)} />
+                    statsToShow.map(s => (
+                        <StatpointProperty key={s.id} name={s.name} value={s.value} increaseEnabled={true} decreaseEnabled={false} calculatedValue={props.calculateFinalStatValue(s)} />
                     ))
                 }
             </div>
--- src/CharacterPanel/SideBar/StatsEditModal.js
+++ src/CharacterPanel/SideBar/StatsEditModal.js
@@ -8,6 +8,8 @@ import ModalFooter from "../../Modal/ModalFooter";
 import ModalHeader from "../../Modal/ModalHeader";
 import ReorderableList from "../../Lists/ReorderableList";
 import { v4 as uuidv4 } from 'uuid';
+import InputCheckbox from "../../Inputs/InputCheckbox";
+import "./StatsEditModal.scss";
 
 export default function StatsEditModal(props) {
     const [stats, setStats] = useState([]);
@@ -25,10 +27,10 @@ export default function StatsEditModal(props) {
         props.onAccept();
     }
 
-    const onChange = (name, index) => {
+    const onChange = (statId, name) => {
         setStats((prevState) => {
-            return prevState.map((s, i) => {
-                if (i === index) {
+            return prevState.map(s => {
+                if (s.id === statId) {
                     return {
                         ...s,
                         name: name
@@ -80,9 +82,10 @@ export default function StatsEditModal(props) {
             return [...prevState, {
                 id: uuidv4(),
                 name: "New stat",
-                value: 5
+                value: 5,
+                isHidden: false
             }];
-        })
+        });
     }
 
     const onClose = () => {
@@ -99,6 +102,20 @@ export default function StatsEditModal(props) {
         setStats(props.stats);
     }
 
+    const isHiddenOnChange = (statId, value) => {
+        setStats((prevState) => {
+            return prevState.map(s => {
+                if (s.id === statId) {
+                    return {
+                        ...s,
+                        isHidden: value
+                    };
+                }
+                return s;
+            });
+        });
+    }
+
     return (
         <Modal show={props.show} onHide={onHide}>
             <ModalHeader onClose={onClose}>
@@ -107,7 +124,12 @@ export default function StatsEditModal(props) {
             <ModalContent>
                 <ReorderableList moveItemUp={moveItemUp} moveItemDown={moveItemDown} deleteItem={deleteItem}>
                     {
-                        stats.map((s, i) => <InputText key={i} value={s.name} onChange={(event) => onChange(event.target.value, i)} />)
+                        stats.map((s, i) => (
+                            <div className="stats-edit-modal__stat" key={s.id}>
+                                <InputText value={s.name} onChange={(event) => onChange(s.id, event.target.value)} className="stats-edit-modal__stat-name" />
+                                <InputCheckbox label={"Hide"} value={s.isHidden} onChange={(_, value) => isHiddenOnChange(s.id, value)} className="stats-edit-modal__stat-hide" />
+                            </div>
+                        ))
                     }
                 </ReorderableList>
                 <button onClick={addNewItem}>Add new stat</button>
